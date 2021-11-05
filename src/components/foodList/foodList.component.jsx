7ed57e3fcd3af36e8350020ec90a396f5d03bd79
Promethesus <jeremyandrews707@gmail.com>
2021-11-05 05:38:51 -0700

    added buttons to menu page for filtering menu along with styling to buttons

--- src/components/foodList/foodList.component.jsx
+++ src/components/foodList/foodList.component.jsx
@@ -4,11 +4,20 @@ import Card from './menuCard'
 
 export const FoodList = props => {
     return (
-        <div className="card-list">
-            {props.food.map(food =>(
-                   <Card key = {food.id} food = {food}></Card>
-            ))
-            }
+        <div className="menu-container">
+            <div className="menu-options">
+                <button className="menu-button">Appetizers</button>
+                <button className="menu-button">Sandwiches</button>
+                <button className="menu-button">Salads</button>
+                <button className="menu-button">Dessert</button>
+                <button className="menu-button">Pizza</button>
+            </div>
+            <div className="card-list">
+                {
+                props.food.map(food =>(
+                    <Card key = {food.id} food = {food}></Card>))
+                }
+            </div>
         </div>
     )
 }
